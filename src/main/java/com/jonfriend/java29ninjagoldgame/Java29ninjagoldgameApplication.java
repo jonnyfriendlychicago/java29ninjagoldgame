71d8291af4b5e7ed3c2533cd04d7be825c1f0b6b
Jon Friend <62109917+jonnyfriendlychicago@users.noreply.github.com>
2022-07-07 10:11:53 -0500

    first commit - template java app running

---
+++ src/main/java/com/jonfriend/java29ninjagoldgame/Java29ninjagoldgameApplication.java
@@ -0,0 +1,13 @@
+package com.jonfriend.java29ninjagoldgame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java29ninjagoldgameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Java29ninjagoldgameApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jonfriend/java29ninjagoldgame/Java29ninjagoldgameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jonfriend.java29ninjagoldgame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Java29ninjagoldgameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
